a54b92cf16e88ec1021c85d349b8ebcd0a2e25de
Mohamed-Trabelsi <74937530+Mohamed-Trabelsi@users.noreply.github.com>
2023-05-13 11:20:04 +0100

    improvements

--- src/main/java/com/example/projectapi/Service/IServiceNode.java
+++ src/main/java/com/example/projectapi/Service/IServiceNode.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 public interface IServiceNode {
 
      ResponseEntity<Object>  getNodesByBarage(Long idBarage);
+     ResponseEntity<Object> getNumNodeByBarrage(Long idBarage);
 
 }
 
